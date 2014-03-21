f4c865fc666f42bc443c3bc9a86cb84259076de5
gustavo hansen <gustavo_1487@hotmail.com>
2014-03-21 17:07:11 -0300

    added file dicovery app

---
+++ sample-file-discovery/src/com/microsoft/filediscovery/viewmodel/ServiceViewItem.java
@@ -0,0 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) Microsoft Open Technologies, Inc.
+ * All Rights Reserved
+ * See License.txt in the project root for license information. 
+ ******************************************************************************/
+package com.microsoft.filediscovery.viewmodel;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class ServiceViewItem.
+ */
+public class ServiceViewItem {
+	
+	public String Name;
+	public String EndpointUri;
+	public String ResourceId;
+	public Boolean Selectable;
+}
